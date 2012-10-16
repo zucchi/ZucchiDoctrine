1a2a965f7e7d9300ef179632a2c0af3e1df3ff67
Matt Cockayne <matt@zucchi.co.uk>
2012-10-16 17:46:04 +0100

    updated entityField Helper to allow recursion

--- src/ZucchiDoctrine/View/Helper/EntityField.php
+++ src/ZucchiDoctrine/View/Helper/EntityField.php
@@ -26,51 +26,61 @@ class EntityField extends AbstractHelper
      *
      * @param AbstractEntity $entity
      * @param string $field
-     * @param 
+     * @param
      * @return mixed
      */
     public function __invoke(AbstractEntity $entity, $field, ClassMetadata $metadata = null)
     {
-        if (!isset($entity->{$field})) {
-            throw new \RuntimeException('Field "' . $field . '" not found in Entity: ' . get_class($entity));
-        }
-        
-        $value = $entity->{$field};
-        
-        if ($metadata) {
-            if ($column = $metadata->getFieldMapping($field)) {
-                switch (strtolower($column['type'])) {
-                    case "money":
-                        $value = $this->getView()->currencyFormat($value, 'GBP');
-                        break;
-                    case "datetime":
-                        $value = $this->getView()->dateFormat(
-                            $value,
-                            \IntlDateFormatter::SHORT,
-                            \IntlDateFormatter::SHORT
-                        );
-                        break;
-                    case "date":
-                        $value = $this->getView()->dateFormat(
-                            $value,
-                            \IntlDateFormatter::SHORT,
-                            \IntlDateFormatter::NONE
-                        );
-                        break;
-                    case "time":
-                        $value = $this->getView()->dateFormat(
-                            $value,
-                            \IntlDateFormatter::NONE,
-                            \IntlDateFormatter::SHORT
-                        );
-                        break;
-                    case 'boolean':
-                        $value = ($value) ? '<span class="true">yes</span>' : '<span class="false">no</span>';
-                        break;
+        if (strpos($field, '->')) {
+            list($Assoc, $field) = explode('->',$field, 2);
+            return $this->__invoke($entity->{$Assoc}, $field);
+
+        } else {
+            if (property_exists('__isInitialised__',$entity) && $entity->__isInitialized__) {
+                // entity is an uninitialised proxy, lets load it
+                $entity->load();
+            }
+            if (!isset($entity->{$field})) {
+                throw new \RuntimeException('Field "' . $field . '" not found in Entity: ' . get_class($entity));
+            }
+
+            $value = $entity->{$field};
+
+            if ($metadata) {
+                if ($column = $metadata->getFieldMapping($field)) {
+                    switch (strtolower($column['type'])) {
+                        case "money":
+                            $value = $this->getView()->currencyFormat($value, 'GBP');
+                            break;
+                        case "datetime":
+                            $value = $this->getView()->dateFormat(
+                                $value,
+                                \IntlDateFormatter::SHORT,
+                                \IntlDateFormatter::SHORT
+                            );
+                            break;
+                        case "date":
+                            $value = $this->getView()->dateFormat(
+                                $value,
+                                \IntlDateFormatter::SHORT,
+                                \IntlDateFormatter::NONE
+                            );
+                            break;
+                        case "time":
+                            $value = $this->getView()->dateFormat(
+                                $value,
+                                \IntlDateFormatter::NONE,
+                                \IntlDateFormatter::SHORT
+                            );
+                            break;
+                        case 'boolean':
+                            $value = ($value) ? '<span class="true">yes</span>' : '<span class="false">no</span>';
+                            break;
+                    }
                 }
             }
         }
-        
+
         return $value;
     }
 }
