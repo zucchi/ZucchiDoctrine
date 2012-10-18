3c683c102272c505956c48b2b528f41315e3e4ed
Matt Cockayne <matt@zucchi.co.uk>
2012-10-18 11:28:23 +0100

    Added ODM traits

---
+++ src/ZucchiDoctrine/DocumentManager/DocumentManagerAwareInterface.php
@@ -0,0 +1,34 @@
+<?php
+/**
+ * ZucchiDoctrine (http://zucchi.co.uk)
+ *
+ * @link      http://github.com/zucchi/ZucchiDoctrine for the canonical source repository
+ * @copyright Copyright (c) 2005-2012 Zucchi Limited. (http://zucchi.co.uk)
+ * @license   http://zucchi.co.uk/legals/bsd-license New BSD License
+ */
+namespace ZucchiDoctrine\DocumentManager;
+
+use Doctrine\ODM\DocumentyManager;
+
+/**
+ * Interface for enforcing Document Manager Awareness
+ *
+ * @author Matt Cockayne <matt@zucchi.co.uk>
+ * @package ZucchiDoctrine
+ * @subpackage DocumentManager
+ */
+interface DocumentManagerAwareInterface
+{
+    /**
+     * set the DocumentManager
+     * @param DocumentManager $em
+     * @return $this
+     */
+    public function setDocumentManager(DocumentManager $em);
+
+    /**
+     * get the currently set DocumentManager
+     * @return DocumentManager
+     */
+    public function getDocumentManager();
+}
---
+++ src/ZucchiDoctrine/DocumentManager/DocumentManagerAwareTrait.php
@@ -0,0 +1,50 @@
+<?php
+/**
+ * ZucchiDoctrine (http://zucchi.co.uk)
+ *
+ * @link      http://github.com/zucchi/ZucchiDoctrine for the canonical source repository
+ * @copyright Copyright (c) 2005-2012 Zucchi Limited. (http://zucchi.co.uk)
+ * @license   http://zucchi.co.uk/legals/bsd-license New BSD License
+ */
+namespace ZucchiDoctrine\DocumentManager;
+
+use Doctrine\ODM\DocumentManager;
+
+/**
+ * Trait providing document manager functionality
+ * 
+ * @author Matt Cockayne <matt@zucchi.co.uk>
+ * @package ZucchiDoctrine
+ * @subpackage DocumentManager
+ */
+
+trait DocumentManagerAwareTrait
+{
+    /**
+     * @var Doctrine\ORM\EntityManager
+     */
+    protected $documentManager;
+
+    /**
+     * set the entity manager
+     * @param EntityManager $em
+     * @return $this
+     */
+    public function setDocumentManager(EntityManager $em)
+    {
+        $this->entityManager = $em;
+        return $this;
+    }
+
+    /**
+     * get the currently set Entity Manager
+     * @return EntityManager
+     */
+    public function getDocumentManager()
+    {
+        if (!$this->documentManager && method_exists($this, 'getServiceManager')) {
+            $this->documentManager = $this->getServiceManager()->get('doctrine.documentmanager.odm_default');
+        }
+        return $this->documentManager;
+    }
+}
